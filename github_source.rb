31fcd61225e2ecd2e695bb413886f176ddede01b
kolmanv <kolmanv@gmail.com>
2014-08-11 02:44:07 +0000

    Added option for multiple github users as source.

--- github_source.rb
+++ github_source.rb
@@ -6,14 +6,18 @@ require 'rufus/scheduler'
 class GithubSource
   def initialize(conf, scheduler)
     @scheduler = scheduler
-    @repos = conf['repos']
-    @user = conf['user']
+    @github = {}
 
-    $logger.info "connecting to github: %s" % conf['token']
-    @github = Github.new do |config|
-      config.oauth_token = conf['token']
+    conf.each do |repo_conf|
+      user = repo_conf['user']
+      @github[user] = {}
+      @github[user]['repos'] = repo_conf['repos']
+      $logger.info "connecting to github: %s" % user
+      @github[user]['connection'] = Github.new do |config|
+        config.oauth_token = repo_conf['token']
+      end
+      $logger.debug @github[user]['connection']
     end
-    $logger.debug @github
 
     scheduler.every '10m', :first_at => :now do |job|
       $logger.info "Loading..."
@@ -37,52 +41,55 @@ class GithubSource
   end
 
   def load
-    @repos.each do |repo|
-      proj_name = repo[0]
-      repo_name = repo[1]
-      $logger.debug "getting repo data: %s/%s" % [@user, repo_name]
-      repo_info = @github.repos.get(@user, repo_name)
+    @github.each do |user,conf|
+      github = conf['connection']
+      conf['repos'].each do |repo|
+        proj_name = repo[0]
+        repo_name = repo[1]
+        $logger.debug "getting repo data: %s/%s" % [user, repo_name]
+        repo_info = github.repos.get(user, repo_name)
 
-      project = SourceProject.new(proj_name)
-      if @projects.has_key?(project.info['id'])
-        @projects[project.info['id']].merge_with(project)
-      else
-        @projects[project.info['id']] = project
-      end
-      project = @projects[project.info['id']]
-      if !project.info.key? 'github_projects'
-        project.info['github_projects'] = []
-      end
-      github_project = {}
-      project.info['github_projects'] << github_project
-      [["name", "full_name"],
-       ["description", "description"],
-       ["url", "html_url"]].each do | info_field, github_field |
-        github_project[info_field] = repo_info.body[github_field]
-      end
-
-      project.info['languages'] = @github.repos.languages(@user, repo_name).to_hash
-
-      contributors = @github.repos.contributors(@user, repo_name)
-      contributors.each do |contributor|
-        $logger.debug "%s contributor for project %s" % [contributor.login, proj_name]
-        member = SourceMember.new(contributor.login)
-        member.projects[project.info['id']] = project
-        if @members.has_key?(member.info['id'])
-          @members[member.info['id']].merge_with(member)
+        project = SourceProject.new(proj_name)
+        if @projects.has_key?(project.info['id'])
+          @projects[project.info['id']].merge_with(project)
         else
-          @members[member.info['id']] = member
+          @projects[project.info['id']] = project
         end
-        project.members[member.info['id']] = @members[member.info['id']]
-      end
-    end 
+        project = @projects[project.info['id']]
+        if !project.info.key? 'github_projects'
+          project.info['github_projects'] = []
+        end
+        github_project = {}
+        project.info['github_projects'] << github_project
+        [["name", "full_name"],
+         ["description", "description"],
+         ["url", "html_url"]].each do | info_field, github_field |
+          github_project[info_field] = repo_info.body[github_field]
+        end
 
-    @members.each do |id, member|
-      user_info = @github.users.get(id)
-      [["avatar_url", "avatar_url"],
-       ["url", "html_url"],
-       ["guthu_public_repos", "public_repos"]].each do | info_field, github_field |
-        member.info[info_field] = user_info.body[github_field]
+        project.info['languages'] = github.repos.languages(user, repo_name).to_hash
+
+        contributors = github.repos.contributors(user, repo_name)
+        contributors.each do |contributor|
+          $logger.debug "%s contributor for project %s" % [contributor.login, proj_name]
+          member = SourceMember.new(contributor.login)
+          member.projects[project.info['id']] = project
+          if @members.has_key?(member.info['id'])
+            @members[member.info['id']].merge_with(member)
+          else
+            @members[member.info['id']] = member
+          end
+          project.members[member.info['id']] = @members[member.info['id']]
+        end
+      end 
+
+      @members.each do |id, member|
+        user_info = github.users.get(id)
+        [["avatar_url", "avatar_url"],
+         ["url", "html_url"],
+         ["github_public_repos", "public_repos"]].each do | info_field, github_field |
+          member.info[info_field] = user_info.body[github_field]
+        end
       end
     end
   end
